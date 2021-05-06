7cd6f2b60d24c60fb06c684c1145044c1d4dfbbc
Mark Nefedov <contact@marknefedov.dev>
2021-05-06 23:33:02 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod config;
+mod index_interface;
+use crate::config::FieldType;
+use crate::index_interface::IndexInterface;
+use anyhow::Result;
+use config::Config;
+use std::collections::HashMap;
+use tantivy::schema::{Schema, INDEXED, STORED, TEXT};
+use tantivy::Index;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = std::fs::File::open("config.yaml")?;
+    let config: Config = serde_yaml::from_reader(config)?;
+    let index_interface = IndexInterface::new(&config)?;
+    Ok(())
+}
